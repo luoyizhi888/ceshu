875222b199874c933ab56b45d712fcd352410973
luoyizhi888 <228465483@qq.com>
2023-07-28 00:23:00 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,8 +1,7 @@
 addEventListener("fetch", event => {
   let url = new URL(event.request.url);
-  if (url.pathname == "/test" && url.search == "") {
-    url.href="https://cachefly.cachefly.net/200mb.test"
-    let request = new Request(url, event.request);
-    event.respondWith(fetch(request));
-  }
+  url.protocol = 'https:'
+  url.hostname = "cachefly.cachefly.net/200mb.test";
+  let request = new Request(url, event.request);
+  event.respondWith(fetch(request));
 })
