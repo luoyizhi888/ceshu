38a9d690aaf1325de6dfdcb49f0b6a3d74a08086
luoyizhi888 <228465483@qq.com>
2023-07-28 00:29:30 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'cachefly.cachefly.net/200mb.test'
+      url.hostname = 'https://cloudflarest.blockly.tk/200mb.zip'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
